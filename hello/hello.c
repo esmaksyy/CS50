e5209a0734ed9f3e081915507f278a09bcec4ae9
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-06 12:06:04 +0000

    Mon, Mar 6, 2023, 3:06 PM +03:00

--- hello/hello.c
+++ hello/hello.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    printf("hello, world\n");
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
 }
-string name = get_string("What's your name? ");
-printf("hello, %s\n", name);
+
 #include <cs50.h>
