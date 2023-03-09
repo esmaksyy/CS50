47ccdbd08156265e8ccf1bbeea920ee097d45411
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-09 14:30:07 +0000

    Thu, Mar 9, 2023, 5:30 PM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,15 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int n;
-
+int main(void)
+{
 do
 {
     n=get_int("Positive Number: ")
 }
 while (n>0 && n<9);
-
 for (int n = 1; n < 9; n++)
 {
     printf("#\n");
 };
+}
