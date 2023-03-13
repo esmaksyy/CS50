d57529297be4cf188476dd69127ef17ae6155276
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-13 09:19:22 +0000

    Mon, Mar 13, 2023, 12:19 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,5 +7,21 @@ do{
     n=get_int("Positive Number: ");
 }
 while (n<1 || n>8);
-
+for (int x = 0; x < n; x++)
+{
+    for (int i = 0; i<n-1-x; i++)
+    {
+        printf(" ");
+    };
+    for (int e =0; e<x+1; e++)
+    {
+        if (e == x)
+        {
+            printf("#\n");
+        }
+        else
+        {
+            printf("#");};
+    };
+};
 }
