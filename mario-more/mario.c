6f7d2f1ea101a08801ff8024d0f6726d13b35667
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-13 11:23:28 +0000

    Mon, Mar 13, 2023, 2:23 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -2,3 +2,26 @@
 #include <stdio.h>
 
 int main(void)
+{int n;
+do{
+    n=get_int("Positive Number: ");
+}
+while (n<1 || n>8);
+for (int x = 0; x < n; x++)
+{
+    for (int i = 0; i<n-1-x; i++)
+    {
+        printf(" ");
+    };
+    for (int e =0; e<x+1; e++)
+    {
+        if (e == x)
+        {
+            printf("#\n");
+        }
+        else
+        {
+            printf("#");};
+    };
+};
+};
