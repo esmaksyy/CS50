ca969ed35666e5eadcb3021f89fa09f3c635d8e9
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-15 15:53:12 +0000

    Wed, Mar 15, 2023, 6:53 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,14 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool check_sum(long num);
+
 int main(void)
 {
-    int numbers;
-    int digits = 0;
-    do
-    {
-        long numbers = get_long("Enter the credit card number here: ");
-        for (numbers > 10)
+    long numbers = get_long("Enter the credit card number here: ");
+
 
 
 
