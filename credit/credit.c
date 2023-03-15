e5be2b648ba875d40c2abc1402e0c4363aa614f7
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-15 17:20:38 +0000

    Wed, Mar 15, 2023, 8:20 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -24,7 +24,7 @@ int main(void)
     printf("digits: %i\n", digits);
     printf("single_digit: %i\n", single_digit);
     printf("two_digit: %i\n", two_digit);
-    if (two_digit == 34 || two_digit == 37 && digits == 15)
+    if ((two_digit == 34 || two_digit == 37) && digits == 15)
     {
         printf("AMEX\n");
         return 0;
