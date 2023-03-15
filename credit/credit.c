eedbf0409737d116d4c1a24ea0c098f896cb7fe7
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-15 14:55:02 +0000

    Wed, Mar 15, 2023, 5:55 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,7 +9,7 @@ int get_long(void)
     {
         numbers = get_long ("Enter numbers here: ");
     }
-    while (numbers < 1000000000000 || numbers > 9999999999999999);
+    while (numbers < 1000000000000 || numbers > 9999999999999999 || );
 
 
 }
