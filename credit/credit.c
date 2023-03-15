5dbfaa36a56ea030c260702136c39cffc0a10487
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-03-15 15:03:34 +0000

    Wed, Mar 15, 2023, 6:03 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,9 +5,11 @@ int main(void)
 int get_long(void)
 {
     int numbers;
+    int digits;
     do
     {
         numbers = get_long ("Enter numbers here: ");
+        digits = numbers%10^12
     }
     while (numbers < 1000000000000 || numbers > 9999999999999999 || );
 
