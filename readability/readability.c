c60d44eeff9ef41b394c43c06614d68a52b31223
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-06-07 16:09:16 +0000

    Wed, Jun 7, 2023, 7:09 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-int main(void) 
+int main(void)
 {
-
+    //Prompt user for text
+    string text = get_string("Text: ");
+    printf("%s\n", text);
 }
