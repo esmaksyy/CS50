10eaa9469736eaaa883c4e32ecaf4840512fcd6c
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-06-08 08:16:36 +0000

    Thu, Jun 8, 2023, 11:16 AM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -32,5 +32,7 @@ int main(void)
     for(int i = 0; i < strlen(text); i++)
     {
         if(text[i] == '.' || text[i] == '!' || text[i] == '?')
+        sentences++;
     }
+    printf("%i sentences\n", sentences);
 }
