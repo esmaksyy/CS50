7b8b54b89fc39d3e5591c27db9195ce51d40ee01
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-06-08 08:14:33 +0000

    Thu, Jun 8, 2023, 11:14 AM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -29,5 +29,8 @@ int main(void)
 
     //Count the number of sentences
     int sentences = 0;
-    
+    for(int i = 0; i < strlen(text); i++)
+    {
+        if(text[i] == '.' || text[i] == '!' || text[i] == '?')
+    }
 }
