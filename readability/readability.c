3ce34f24e4b3b5fddbb2ba00c90bbead1c29377c
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-06-08 07:48:18 +0000

    Thu, Jun 8, 2023, 10:48 AM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -12,5 +12,7 @@ int main(void)
     //Count the number of letters there are in the text
     int letters = 0;
     for(int i = 0; i < strlen(text); i++)
-    
+    {
+        if((text[i] >= 'a' ))
+    }
 }
