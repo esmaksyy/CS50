2ec9a4ffa1cdc50a50c9e297875bfddb833f8fb4
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-06-14 06:50:56 +0000

    Wed, Jun 14, 2023, 9:50 AM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -12,15 +12,19 @@ int main(int argc, string argv[])
         return 1;
     }
     else printf("Success!\n");
+
     //Define the key
     string key = argv[1];
 
     //Check that the input is a digit
     for (int i = 0; i < strlen (argv[1]); i++)
-    if (isdigit(argv[1][i]))
     {
-        printf("Usage: ./caesar key\n");
-        return 1;
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
     }
-    
+    printf("Success\n%s\n", key);
+
 }
