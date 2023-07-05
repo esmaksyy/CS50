b53a54beea0088fdee16bbc880082a7b481be5fd
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-07-05 14:47:56 +0000

    Wed, Jul 5, 2023, 5:47 PM +03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,8 +71,8 @@ bool vote(string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-                    candidates[i].votes++;
-                    return true;
+            candidates[i].votes++;
+            return true;
         }
     }
     return false;
