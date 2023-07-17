2ff2f9d9c28733715b9949869b8fef63be177adc
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-07-17 17:16:44 +0000

    Mon, Jul 17, 2023, 8:16 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -191,6 +191,15 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
+    int eliminate = 0;
+    int counter = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated)
+        {
+            eliminate++;
+        }
+    }
     return false;
 }
 
