6623bc6e2c295b137fb4f7a003a65cc7875b3f32
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-07-31 17:39:47 +0000

    Mon, Jul 31, 2023, 8:39 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -129,6 +129,13 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        for(int j = 0; j < candidate_count; j++)
+        {
+            if(preferences[i][j] > )
+        }
+    }
     return;
 }
 
