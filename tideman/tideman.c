6fd584ad19f28f63c2c8e7cc7f5486f218fc5071
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-07-31 23:42:38 +0000

    Tue, Aug 1, 2023, 2:42 AM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -215,7 +215,7 @@ void print_winner(void)
     // TODO
     for(int i = 0; i < candidate_count; i++)
     {
-        bool is_banana = true
+        bool is_banana = true;
 
         for (int j = 0; j < candidate_count; j++)
         {
