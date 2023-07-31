8e6a1f8bd2ce18346faedd36a87e52d2ca6cd389
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-07-31 17:15:58 +0000

    Mon, Jul 31, 2023, 8:15 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,6 +100,13 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp(name, candidates[i]) == 0)
+        {
+            
+        }
+    }
     return false;
 }
 
