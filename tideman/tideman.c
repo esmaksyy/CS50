b1db7da3f2add62080dcdf7de6b0940ebaf4532a
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-07-31 23:38:51 +0000

    Tue, Aug 1, 2023, 2:38 AM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -214,5 +214,23 @@ void print_winner(void)
 {
     // TODO
     for(int i = 0; i < candidate_count; i++)
+    {
+        bool is_banana = true
+
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (locked[j][i])
+            {
+                is_banana = false;
+                break;
+            }
+        }
+
+        if(is_banana)
+        {
+            printf("%s\n", candidates[i]);
+            return;
+        }
+    }
     return;
 }
