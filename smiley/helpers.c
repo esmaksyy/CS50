904893f0da39eb64af53eab955f46ef50203ccd6
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-08-29 14:41:44 +0000

    Tue, Aug 29, 2023, 5:41 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,7 +7,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int column = 0; column < height; column++)
         {
-            if(image[column][row].rhbtBlue = 0)
+            if(image[column][row].rhbtBlue == 0 && image[column][row].rhbtGreen == 0 && image[column][row].rhbtRed == 0)
         }
     }
 }
