e8ba8dc20565b1ab405d0ebbac5b1ea83cff2f45
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-08-29 14:38:39 +0000

    Tue, Aug 29, 2023, 5:38 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -4,4 +4,10 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
     for(int row = 0; row < width; row++)
+    {
+        for(int column = 0; column < height; column++)
+        {
+            if(image[column][row].rhbtBlue = 0)
+        }
+    }
 }
