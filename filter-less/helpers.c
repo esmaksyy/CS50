f0efde16a670c69baf05c458a0d0adab08fdb8a8
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-08-30 13:46:19 +0000

    Wed, Aug 30, 2023, 4:46 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -72,7 +72,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
     {
         // Comb through each column
-        for (int j = 0)
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image
+        }
     }
     return;
 }
