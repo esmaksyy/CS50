6c2cd6ca06f44e1fe651e5fbe03233ef073a7f0c
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-08-30 14:18:39 +0000

    Wed, Aug 30, 2023, 5:18 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -85,5 +85,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a copy of the image
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp[i][j] = image[i][j];
+        }
+    }
     return;
 }
