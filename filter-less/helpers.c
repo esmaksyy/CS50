81532a462c3ded4e103b7cda63a4fc4bb54fe52b
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-08-30 10:38:32 +0000

    Wed, Aug 30, 2023, 1:38 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,8 +1,19 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Comb through each row
+    for (int i = 0; i < height; i++)
+    {
+        // Comb through each column
+        for (int j = 0; j < width; j++)
+        {
+            // Convert pixels to float
+            
+        }
+    }
     return;
 }
 
