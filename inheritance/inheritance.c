4077f2cb1cef05797c550bd2df7a5520699aec90
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-16 10:42:28 +0000

    Mon, Oct 16, 2023, 1:42 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -54,6 +54,7 @@ person *create_family(int generations)
         p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        p->alleles[0] = p->parents[0]->alleles[rand()%2];
 
     }
 
