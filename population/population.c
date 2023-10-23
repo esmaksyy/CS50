d1a6989624030af6d9b19f3039c4c69c9175413a
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-23 14:13:59 +0000

    Mon, Oct 23, 2023, 5:13 PM +03:00

--- population/population.c
+++ population/population.c
@@ -23,7 +23,7 @@ int main(void)
     // TODO: Calculate number of years until we reach threshold
     while (start < end)
     {
-        start = start + start / 0.625 - start / ;
+        start = start + start / 0.625 - start / 5;
         years++;
     }
 
