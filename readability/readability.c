0e3e1a62e93efaa53aec0a01d4cef5e55caad253
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-23 13:23:14 +0000

    Mon, Oct 23, 2023, 4:23 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,38 +1,38 @@
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 
 int main(void)
 {
-    //Prompt user for text
+    // Prompt user for text
     string text = get_string("Text: ");
 
-    //Count the number of letters there are in the text
+    // Count the number of letters there are in the text
     int letters = 0;
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
-        if((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
-        letters++;
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+            letters++;
     }
 
-    //Count the number of words
+    // Count the number of words
     int words = 1;
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
-        if(text[i] == ' ')
-        words++;
+        if (text[i] == ' ')
+            words++;
     }
 
-    //Count the number of sentences
+    // Count the number of sentences
     int sentences = 0;
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
-        if(text[i] == '.' || text[i] == '!' || text[i] == '?')
-        sentences++;
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+            sentences++;
     }
 
-    //Use Coleman-Liau index to determine the reading grade level
+    // Use Coleman-Liau index to determine the reading grade level
     float calculation = (0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8;
     int index = round(calculation);
 
