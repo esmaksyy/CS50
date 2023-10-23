cc4cd7d2ac7faaf5088d294827c3cae7a9625ddf
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-23 13:00:04 +0000

    Mon, Oct 23, 2023, 4:00 PM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,27 +3,28 @@
 
 int main(void)
 {
-int n;
-do
-{
-    n=get_int("Positive Number: ");
-}
-while (n<1 || n>8);
-for (int x = 0; x < n; x++)
-{
-    for (int i = 0; i<n-1-x; i++)
+    int n;
+    do
     {
-        printf(" ");
-    };
-    for (int e =0; e<x+1; e++)
+        n = get_int("Positive Number: ");
+    }
+    while (n < 1 || n > 8);
+    for (int x = 0; x < n; x++)
     {
-        if (e == x)
+        for (int i = 0; i < n - 1 - x; i++)
         {
-            printf("#\n");
-        }
-        else
+            printf(" ");
+        };
+        for (int e = 0; e < x + 1; e++)
         {
-            printf("#");};
+            if (e == x)
+            {
+                printf("#\n");
+            }
+            else
+            {
+                printf("#");
+            };
+        };
     };
-};
 };
