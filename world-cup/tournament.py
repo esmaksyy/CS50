8b0366bdee7f060b5aca8f633c5b82dd9d2fed35
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-24 21:39:38 +0000

    Wed, Oct 25, 2023, 12:39 AM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -48,7 +48,7 @@ def main():
             print(f"{team}: {counts[team] * 100 / n:.1f}% chance of winning")
 
         # Printf time taken
-        print(f"Elapsed time: (elapsed_time: .3f)s")
+        print(f"Elapsed time: {elapsed_time: .3f}s")
         print()
 
 
