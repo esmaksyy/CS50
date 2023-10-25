03cc23c3e7563a445f23f9894c3b0e3141de2ffb
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-25 16:52:15 +0000

    Wed, Oct 25, 2023, 7:52 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -15,3 +15,20 @@ count = 0
 while cents >= 25:
     cents = cents - 25
     count += 1
+
+# Number of dimes
+while cents >= 10:
+    cents = cents - 10
+    count += 1
+
+# Number of nickels
+while cents >= 5:
+    cents = cents - 5
+    count += 1
+
+#Number of dimes
+while cents >= 1:
+    cents = cents - 1
+    count += 1
+
+print("Total coins: ", count)
