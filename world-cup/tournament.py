09d3cf093b70d152294e6f65f900174524f16d3f
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-25 13:22:40 +0000

    Wed, Oct 25, 2023, 4:22 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -7,6 +7,7 @@ import random
 # Number of simluations to run
 N = 1000
 
+
 def main():
     # Ensure correct usage
     if len(sys.argv) != 2:
@@ -17,9 +18,9 @@ def main():
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for row in reader:
-            team_name = row['team']
-            team_rating = int(row['rating'])
-            teams.append({'team': team_name, 'rating': team_rating})
+            team_name = row["team"]
+            team_rating = int(row["rating"])
+            teams.append({"team": team_name, "rating": team_rating})
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
@@ -31,10 +32,10 @@ def main():
         else:
             counts[winner_team] = 1
 
-
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
+
 
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
@@ -62,7 +63,7 @@ def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     while len(teams) > 1:
         teams = simulate_round(teams)
-    return teams[0]['team']
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
