d6d6126d54128c76aecfced42c7f7177c3ee9332
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-25 16:30:09 +0000

    Wed, Oct 25, 2023, 7:30 PM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -9,7 +9,7 @@ while True:
 
 for i in range(0, n, 1):
     for j in range(0, n, 1):
-        if (i + j < n - 1):
+        if i + j < n - 1:
             print(" ", end="")
         else:
             print("#", end="")
