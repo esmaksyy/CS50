ecffd2d7cd59d0854fa0aca3e4cbb29146f743d3
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-25 11:56:07 +0000

    Wed, Oct 25, 2023, 2:56 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -31,7 +31,7 @@ def main():
 
         # TODO: Simulate N tournaments and keep track of win counts # Determine the winner of the tournament # Increment the team's win count in the counts dictionary
         for i in range(N):
-            winning_team = simulate_tournament(teams)
+            winner_team = simulate_tournament(teams)
             if winning_team in counts:
                 counts[winning_team] += 1
             else:
