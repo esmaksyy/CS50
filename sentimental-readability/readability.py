120d97b5ffa8fc6cdd2e388aa739414ff3356bfe
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-10-26 08:56:02 +0000

    Thu, Oct 26, 2023, 11:56 AM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,4 +6,14 @@ from cs50 import get_string
 text = get_string("Text: ")
 
 letters = 0
-words = 
+words = 1
+sentences = 0
+
+# Count number of letters, words and sentences
+for i in text:
+    if i.isalpha():
+        letters += 1
+    elif i == " ":
+        words += 1
+    elif i == "." or i == "!" or i == "?":
+        sentences += 1
