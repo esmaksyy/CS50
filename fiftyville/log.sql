128ab727bdec03a9b152edb3877bc8e0aea6f9dd
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-11-04 22:37:12 +0000

    Sun, Nov 5, 2023, 1:37 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,4 +3,5 @@
 SELECT description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28;
 
-SELECT transcript FROM 
+SELECT transcript FROM interviews
+WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
