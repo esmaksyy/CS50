d1225b355eb8c9039eb1b0df3000f3407811f93a
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-11-04 20:09:44 +0000

    Sat, Nov 4, 2023, 11:09 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -6,4 +6,5 @@ WHERE movies.id IN
 (SELECT movies.id FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON people.id = stars.person_id
-WHERE people.name = "Kevin Bacon" AND people.birth)
+WHERE people.name = "Kevin Bacon" AND people.birth = "1958")
+AND people.name != "Kevin Bacon";
