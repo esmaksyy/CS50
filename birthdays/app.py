de9327878a9bec1b5ad68ebb5b3e7392af8faa3f
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-11-23 14:57:59 +0000

    Thu, Nov 23, 2023, 5:57 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -28,7 +28,10 @@ def index():
 
         # TODO: Add the user's entry into the database
         name = request.form.get("name")
-        month = request.form.
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        # Insert 
 
         return redirect("/")
 
