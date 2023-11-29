29e4eef9f7db2e73a636049910adde5dd8a7d439
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-11-29 00:20:37 +0000

    Wed, Nov 29, 2023, 3:20 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -109,9 +109,10 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    if (request.method == 'POST')
-    return apology("TODO")
-
+    if (request.method == 'POST'):
+        pass
+    else:
+        return render_template("register.html")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
