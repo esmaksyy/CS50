65f6dda6e3515bf752d02f2c81ad1ae4c708ccc5
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-11-29 21:54:22 +0000

    Thu, Nov 30, 2023, 12:54 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -113,7 +113,7 @@ def quote():
         if not item:
             return apology("Invalid symbol!")
 
-        return render_template("quoted.html", item=item)
+        return render_template("quoted.html", quoted=item)
     else:
         return render_template("quote.html")
 
