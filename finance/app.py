082dea5572137a58ceb8a06ea55b60abb1de8b2d
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-11-30 13:11:55 +0000

    Thu, Nov 30, 2023, 4:11 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -35,7 +35,7 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    user_id = session["user_id"]
 
 
 @app.route("/buy", methods=["GET", "POST"])
