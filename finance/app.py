0d88e6afd528d26167ab934425bb1a6cedc10cf5
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-11-30 21:46:57 +0000

    Fri, Dec 1, 2023, 12:46 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -39,6 +39,9 @@ def index():
 
     stocks = db.execute("SELECT symbol, name, price, SUM(shares) FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
     cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
+
+    total = cash
+
     
 
     return render_template("index.html", stocks=stocks, cash=cash, usd=usd, )
