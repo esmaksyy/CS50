acaf7e9feb70d4b1046d7ca4e1f9417243759c81
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-12-01 11:18:07 +0000

    Fri, Dec 1, 2023, 2:18 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -91,7 +91,10 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    user_id = session["user_id"]
+    transactions = db.execute("SELECT type, symbol, price, shares, time FROM transactions WHERE user_id = ?", user_id)
+
+    return render_template("history.html", transactions=transactions,)
 
 
 @app.route("/login", methods=["GET", "POST"])
