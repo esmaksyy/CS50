2029f6fa9382f163c06275dde1b9b3a05728d761
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-12-15 08:15:57 +0000

    Fri, Dec 15, 2023, 11:15 AM +03:00

--- project/app.py
+++ project/app.py
@@ -23,11 +23,11 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///project.db")
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+#if not os.environ.get("API_KEY"):
+#    raise RuntimeError("API_KEY not set")
 
 
 @app.after_request
