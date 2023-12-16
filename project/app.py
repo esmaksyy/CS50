dc273005e89033684d05ac3521dac4afaf16097b
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-12-16 11:30:09 +0000

    Sat, Dec 16, 2023, 2:30 PM +03:00

--- project/app.py
+++ project/app.py
@@ -78,7 +78,7 @@ def sent():
     usernameDB =db.execute("SELECT username FROM users WHERE id = ?", userId)
     username = usernameDB[0]["username"]
     emails = db.execute("SELECT * FROM emails WHERE sender = ?", username)
-    return jsonify(emails)
+    return render_template()
 
 
 @app.route("/login", methods=["GET", "POST"])
