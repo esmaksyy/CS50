01635c28c3de115e83b914981c722cf382312c2b
esmaksyy <126894002+esmaksyy@users.noreply.github.com>
2023-12-16 18:48:36 +0000

    Sat, Dec 16, 2023, 9:48 PM +03:00

--- project/app.py
+++ project/app.py
@@ -5,8 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required, usd
 
 # Configure application
 app = Flask(__name__)
